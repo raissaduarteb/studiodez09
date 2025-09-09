d1b6b048794c049a87fb262be1eba279e055c825
Raissa Duarte Braga <raissaduarteb@192.168.3.27>
2025-09-09 14:28:26 -0300

    novo commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+fetch('footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+  });
+
+  fetch('header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header').innerHTML = data;
+  });
