d96ad6a1774c35bddbe199c61fab08223c829fd3
Raissa Duarte Braga <raissaduarteb@192.168.3.34>
2025-09-22 23:59:11 -0300

    responsividade

--- js/script.js
+++ js/script.js
@@ -12,15 +12,12 @@ fetch('footer.html')
 
 // Pega o caminho da URL atual
 const currentPage = window.location.pathname.split("/").pop();
-console.log(currentPage);
 
 // Seleciona todos os links do navbar
 setTimeout(() => {
   const navLinks = document.querySelectorAll(".header-nav li a");
-  console.log(navLinks)
   navLinks.forEach(link => {
     const linkPage = link.getAttribute("href").replaceAll('/', '');
-    console.log(linkPage);
     if (linkPage === currentPage) {
       link.parentElement.classList.add("active");
     }
